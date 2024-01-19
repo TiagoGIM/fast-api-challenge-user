5a29fcae5c1eb9a6ff768f8f84e1d06199c0a22d
tiagoGIM <herique.sa@gmail.com>
2024-01-19 04:35:55 -0300

    feat: register new user implemented

--- app/user/user_router.py
+++ app/user/user_router.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from ..user.user_service import UserService
+from ..depends import get_db_session
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 from ..user.user_schemas import UserCreateDTO
 
 router_user = APIRouter(
@@ -6,6 +9,11 @@ router_user = APIRouter(
     tags=["users"]
 )
 
-@router_user.post("/", status_code=201)
-def create_user(user : UserCreateDTO):
+@router_user.post("/singin", status_code=201,)
+def create_user(
+    user : UserCreateDTO,
+    db_session: Session = Depends(get_db_session)
+                ):
+    user_service = UserService(db_session)
+    user_service.register(user)
     return {'message':'User created successfully', 'user' : user}
--- app/user/user_service.py
+++ app/user/user_service.py
@@ -1,15 +1,29 @@
+from fastapi import status
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+from ..authentication.auth_security import get_password_hash
+from ..db.models import UserModel
 from ..user.user_repository import UserRepository
-from ..user.user_schemas import User, UserCreateDTO
+from ..user.user_schemas import UserCreateDTO
 
 
 class UserService:
-    def __init__(self):
-        self.user_repository = UserRepository(db = '')
+    def __init__(self, db: Session):
+        self.user_repository = UserRepository(db)
 
-    def registre(self, user_dto: UserCreateDTO):
-        user = User(**user_dto.dict())
-        if len(user_dto.password) < 8:
-            raise Exception("Password must be at least 8 characters long")
+    def register(self, user_dto: UserCreateDTO):
 
-        return self.user_repository.create(user)
+        userToSave = UserModel(
+            username = user_dto.username,
+            email = user_dto.email,
+            password = get_password_hash(user_dto.password)
+        )
+        try:
+            self.user_repository.create(userToSave)
+        except IntegrityError:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='User already exists'
+            )
     
