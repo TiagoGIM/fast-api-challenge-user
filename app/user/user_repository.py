7cb55bc47054627adb7b87199798d08e01230711
tiagoGIM <herique.sa@gmail.com>
2024-01-19 05:27:24 -0300

    fix: all crud methods implemented

--- app/user/user_repository.py
+++ app/user/user_repository.py
@@ -1,6 +1,7 @@
 from ..db.models import UserModel
 from ..user.user_schemas import User
 from sqlalchemy.orm import Session
+from typing import List
 
 class UserRepository:
     def __init__(self, db: Session):
@@ -16,3 +17,22 @@ class UserRepository:
         found_user = self.db.query(UserModel).filter(UserModel.username == username).first()
 
         return found_user if found_user else None
+    
+    def get_all(self) -> List[UserModel]:
+        return self.db.query(UserModel).all()
+    
+    def update(self, user : UserModel) -> User:
+        self.db.merge(user)
+        self.db.commit()
+        self.db.flush()
+        return user.to_user()
+
+    def delete(self, user_id):
+        user = self.db.query(UserModel).filter(UserModel.id == user_id).first()
+        if user:
+            self.db.delete(user)
+            self.db.commit()
+            self.db.flush()
+            return True
+
+        return False
